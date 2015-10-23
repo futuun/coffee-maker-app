6ecfb0d0f8bb15e5f1890a67f7d96b627b03dbf4
Michal Mokijewski <mokijewskim@gmail.com>
2015-10-23 13:48:44 +0200

    add 1st 'child' component (cup size)

---
+++ src/size.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './size.scss'
+
+const Size = React.createClass({
+  render () {
+    return (
+      <div className="cup-size">
+        <h1>todo</h1>
+      </div>
+    )
+  }
+})
+
+export default Size
